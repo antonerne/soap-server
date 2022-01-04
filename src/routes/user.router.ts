724cf3274df464a72415a5f90e9a9a8937812a45
Anton P Erne <79037362+antonerne@users.noreply.github.com>
2022-01-04 15:57:45 +0900

    Jan04-1557

---
+++ src/routes/user.router.ts
@@ -0,0 +1,96 @@
+// External Dependencies
+import express, {Request, Response} from 'express';
+import { ObjectId } from 'mongodb';
+import { collections } from '../services/database.service';
+import { IUser, User } from 'soap-model'
+
+// Global Config
+export const userRouter = express.Router();
+userRouter.use(express.json());
+
+// Get - retries a single user information
+// Get all users
+userRouter.get("/", async(req: Request, res: Response) => {
+    try {
+        if (collections.users) {
+            const users = (await collections.users.find({}).toArray()) as IUser[];
+            res.status(200).send(users);
+            return
+        }
+        res.status(404).send("No users found in database!");
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).send(error.message);
+            return
+        }
+        res.status(500).send(error);
+    }
+});
+
+// get user from id
+userRouter.get("/:id", async(req: Request, res: Response) => {
+    const id = req.params.id;
+
+    try {
+        const query = { _id: new ObjectId(id)};
+        
+        if (collections.users) {
+            const user = (await collections.users.findOne(query)) as IUser;
+            res.status(200).send(user);
+            return
+        }
+        res.status(404).send(`User: ${id} not found in database!`);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).send(error.message);
+            return
+        }
+        res.status(500).send(error);
+    }
+})
+
+// Post - two functions
+// 1)  Create a new user
+userRouter.post("/", async(req: Request, res: Response) => {
+    try {
+        const tuser = req.body as IUser
+        const newuser: User = new User(tuser);
+
+        const query = { email: tuser.email }
+        const ouser = (await collections.users?.findOne(query)) as IUser;
+
+        if (!ouser) {
+            const result = await collections.users?.insertOne(newuser);
+
+            result 
+                ? res.status(202).send(`Successfully create new user with id: ${result.insertedId}`)
+                : res.status(500).send("Failed to create a new user");
+        } else {
+            // user for email address already in system.
+            res.status(403).send(`User already present for email address: ${tuser.email}`);
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).send(error.message);
+            return
+        }
+        res.status(500).send(error);
+    }
+});
+
+// 2)  Authenticate user
+userRouter.post("/login/", async(req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).send(error.message);
+            return
+        }
+        res.status(500).send(error);
+    }
+});
+
+// Put - update the user, to include password
+
+// Delete - delete a user, along with their respective soap entries
---
+++ src/services/database.service.ts
@@ -0,0 +1,31 @@
+// External Dependencies
+import * as mongo from 'mongodb';
+import * as dotenv from 'dotenv';
+
+// Global Variables
+export const collections: { entries?: mongo.Collection, 
+    users?: mongo.Collection, readings?: mongo.Collection } = {}
+
+// Initialize Connection
+export async function connectToDatabase() {
+    dotenv.config();
+
+    const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWD}`
+        + `@${process.env.MONGO_SERVER}:${process.env.MONGO_PORT}`;
+
+    const client: mongo.MongoClient = new mongo.MongoClient(uri);
+
+    await client.connect();
+
+    const db: mongo.Db = client.db(process.env.MONGO_DB_NAME);
+
+    const usersCollection: mongo.Collection = db.collection("users");
+    collections.users = usersCollection;
+
+    const entriesCollection: mongo.Collection = db.collection("entries");
+    collections.entries = entriesCollection;
+
+    const readingsCollection: mongo.Collection = db.collection("readings");
+
+    console.log(`Successfully connected to database: ${db.databaseName}`);
+}
